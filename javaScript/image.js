94d801777ecc633b715d862a93ae822ef93f29b1
Ashish kumar anand <ashishraj5296@gmail.com>
2022-07-06 18:38:34 +0530

    first commit

---
+++ javaScript/image.js
@@ -0,0 +1,45 @@
+function openImg1(){
+    var image1 = document.getElementById('image1');
+    var source = image1.src;
+    window.open(source);
+}
+function openImg2(){
+    var image2 = document.getElementById('image2');
+    var source = image2.src;
+    window.open(source);
+}
+function openImg3(){
+    var image3 = document.getElementById('image3');
+    var source = image3.src;
+    window.open(source);
+}
+function openImg4(){
+    var image4 = document.getElementById('image4');
+    var source = image4.src;
+    window.open(source);
+}
+function openImg5(){
+    var image5 = document.getElementById('image5');
+    var source = image5.src;
+    window.open(source);
+}
+function openImg6(){
+    var image6 = document.getElementById('image6');
+    var source = image6.src;
+    window.open(source);
+}
+function openImg7(){
+    var image7 = document.getElementById('image7');
+    var source = image7.src;
+    window.open(source);
+}
+function openImg8(){
+    var image8 = document.getElementById('image8');
+    var source = image8.src;
+    window.open(source);
+}
+function openImg9(){
+    var image9 = document.getElementById('image9');
+    var source = image9.src;
+    window.open(source);
+}
